ed2d1733b2005060cc9b8fca9e13b57d841f9a5b
hayoung10 <mhymsy@gmail.com>
2022-12-21 20:39:00 +0900

    Programmers - Level 3 - 정수 삼각형

---
+++ Programmers/src/Level3/Integer_Triangle.java
@@ -0,0 +1,27 @@
+package Level3;
+
+// DP 동적계획법
+
+public class Integer_Triangle {
+    public int solution(int[][] triangle) {
+        int answer = 0;
+        int[][] dp = new int[triangle.length][triangle.length];
+        dp[0][0] = triangle[0][0];
+
+        for(int i = 1; i < triangle.length; i++) {
+            dp[i][0] = triangle[i][0] + dp[i - 1][0];
+
+            for(int j = 1; j <= i; j++) {
+                dp[i][j] = triangle[i][j] + Math.max(dp[i - 1][j - 1], dp[i - 1][j]);
+            }
+
+            dp[i][i] = triangle[i][i] + dp[i - 1][i - 1];
+        }
+
+        for(int i = 0; i < triangle.length; i++) {
+            answer = Math.max(answer, dp[triangle.length - 1][i]);
+        }
+
+        return answer;
+    }
+}
