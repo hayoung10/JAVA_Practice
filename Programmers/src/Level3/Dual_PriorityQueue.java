011bda8ed374b6117805d80966f566fd1b255971
hayoung10 <mhymsy@gmail.com>
2022-12-23 17:01:24 +0900

    Programmers - Level 3 - 이중우선순위큐

---
+++ Programmers/src/Level3/Dual_PriorityQueue.java
@@ -0,0 +1,41 @@
+package Level3;
+
+import java.util.*;
+
+// Heap 힙
+
+public class Dual_PriorityQueue {
+    public int[] solution(String[] operations) {
+        int[] answer = {0, 0};
+
+        PriorityQueue<Integer> minPQ = new PriorityQueue<>();
+        PriorityQueue<Integer> maxPQ = new PriorityQueue<>(Collections.reverseOrder()); // 내림차순
+
+        for(String op : operations) {
+            if(op.charAt(0) == 'I') { // 주어진 숫자 삽입
+                int num = Integer.parseInt(op.substring(2));
+                maxPQ.add(num);
+                minPQ.add(num);
+            } else {
+                if(!maxPQ.isEmpty()) {
+                    if(op.charAt(2) == '-') { // 최솟값 삭제
+                        int minNum = minPQ.peek();
+                        maxPQ.remove(minNum);
+                        minPQ.remove(minNum);
+                    } else { // 최댓값 삭제
+                        int maxNum = maxPQ.peek();
+                        maxPQ.remove(maxNum);
+                        minPQ.remove(maxNum);
+                    }
+                }
+            }
+        }
+
+        if(!maxPQ.isEmpty()) {
+            answer[0] = maxPQ.peek();
+            answer[1] = minPQ.peek();
+        }
+
+        return answer;
+    }
+}
