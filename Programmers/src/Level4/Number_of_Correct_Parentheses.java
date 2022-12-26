812a08d63b91d852f474a0d315e02afa536cbe47
hayoung10 <mhymsy@gmail.com>
2022-12-27 02:57:16 +0900

    Programmers - Level 4 - 올바른 괄호의 갯수

---
+++ Programmers/src/Level4/Number_of_Correct_Parentheses.java
@@ -0,0 +1,18 @@
+package Level4;
+
+public class Number_of_Correct_Parentheses {
+    // DP 동적계획법
+    public int solution(int n) {
+        int dp[] = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                dp[i] += dp[i - j] * dp[j - 1];
+            }
+        }
+
+        return dp[n];
+    }
+}
