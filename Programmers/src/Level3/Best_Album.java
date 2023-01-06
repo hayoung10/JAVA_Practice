2f00cf5c15f1b108ece3aa23b9defce743f938d5
hayoung10 <mhymsy@gmail.com>
2023-01-07 00:32:10 +0900

    Programmers - Level 3 - 베스트앨범 (modify)

--- Programmers/src/Level3/Best_Album.java
+++ Programmers/src/Level3/Best_Album.java
@@ -6,19 +6,36 @@ import java.util.*;
 
 public class Best_Album { // 베스트앨범
     public int[] solution(String[] genres, int[] plays) {
-        int[] answer = {};
+        ArrayList<Integer> answer = new ArrayList<>();
 
-        HashMap<String, Integer> map = new HashMap<>(); // 장르별 재생횟수
-        HashMap<String, HashMap<Integer, Integer>> music = new HashMap<>();
+        HashMap<String, Integer> genres_plays = new HashMap<>(); // (장르, 총 재생 횟수)
+        HashMap<String, HashMap<Integer, Integer>> music_plays = new HashMap<>(); // (장르, (고유 번호, 재생 횟수))
+
+        // 초기화
         for(int i = 0; i < plays.length; i++) {
-            if()
+            if(!genres_plays.containsKey(genres[i])) {
+                HashMap<Integer, Integer> map = new HashMap<>();
+                map.put(i, plays[i]);
+                music_plays.put(genres[i], map);
+            } else {
+                music_plays.get(genres[i]).put(i, plays[i]);
+            }
+            genres_plays.put(genres[i], genres_plays.getOrDefault(genres[i], 0) + plays[i]);
         }
-        for(int i = 0; i < genres.length; i++)
-            map.put(genres[i], map.getOrDefault(genres[i], 0)+ plays[i]);
 
-        List<String> keySet = new ArrayList<>(map.keySet());
-        Collections.sort(keySet, (a, b) -> map.get(b) - map.get(a));
+        List<String> genre_keySet = new ArrayList(genres_plays.keySet());
+        Collections.sort(genre_keySet, (a, b) -> genres_plays.get(b) - genres_plays.get(a)); // 내림차순 정렬
 
-        return answer;
+        for(String genre : genre_keySet) {
+            HashMap<Integer, Integer> map = music_plays.get(genre);
+            List<Integer> music_keySet = new ArrayList(map.keySet());
+            Collections.sort(music_keySet, (a, b) -> map.get(b) - map.get(a)); // 내림차순 정렬
+
+            answer.add(music_keySet.get(0));
+            if(music_keySet.size() > 1)
+                answer.add(music_keySet.get(1));
+        }
+
+        return answer.stream().mapToInt(Integer::intValue).toArray(); // ArrayList -> Array
     }
 }
