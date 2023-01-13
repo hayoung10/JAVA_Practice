66f5f6f8c0e24e7c78cbd284eb455fb6c7730da7
hayoung10 <mhymsy@gmail.com>
2023-01-14 00:43:09 +0900

    Programmers - Level 3 - 아이템 줍기(Case 2)

--- Programmers/src/Level3/Pick_up_Item.java
+++ Programmers/src/Level3/Pick_up_Item.java
@@ -5,7 +5,8 @@ import java.util.*;
 // BFS 너비 우선 탐색
 
 public class Pick_up_Item { // 아이템 줍기
-    public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
+    // Case 1
+    public int solution1(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
         int answer = 0;
 
         int[][] map = new int[103][103];
@@ -52,6 +53,60 @@ public class Pick_up_Item { // 아이템 줍기
         }
 
         return answer / 2;
+    }
+
+    // Case 2
+    private String[][] map = new String[51][51];
+    private int[] xy;
+    private int answer = 0, step = 0;
+
+    public int solution2(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
+        // 초기화
+        xy = new int[] {characterX, characterY, itemX, itemY};
+
+        for(int i = 0; i < 51; i++)
+            Arrays.fill(map[i], "");
+
+        for(int[] arr : rectangle) {
+            int lx = arr[0], ly = arr[1], rx = arr[2], ry = arr[3];
+
+            map[lx][ly] = "LDP"; // Left Down Point
+            map[lx][ry] = "LUP"; // Left Up Point
+            map[rx][ry] = "RUP"; // Right Up Point
+            map[rx][ly] = "RDP"; // Right Down Point
+
+            for(int i = lx + 1; i < rx; i++) {
+                map[i][ly] += "D"; // Down Side
+                map[i][ry] += "U"; // Up Side
+            }
+            for(int j = ly + 1; j < ry; j++) {
+                map[lx][j] += "L"; // Left Side
+                map[rx][j] += "R"; // Right Side
+            }
+        }
+        BFS(characterX, characterY);
+
+        return answer;
+    }
+
+    private void BFS(int x, int y) {
+        String loc = map[x][y];
+        if(loc.equals("LDP") || loc.equals("LU") || loc.equals("UL") || loc.equals("L")) y++;
+        if(loc.equals("RUP") || loc.equals("RD") || loc.equals("DR") || loc.equals("R")) y--;
+        if(loc.equals("LUP") || loc.equals("RU") || loc.equals("UR") || loc.equals("U")) x++;
+        if(loc.equals("RDP") || loc.equals("LD") || loc.equals("DL") || loc.equals("D")) x--;
+        step++;
+
+        if(x == xy[0] && y == xy[1]) {
+            answer = Math.min(answer, step);
+            return;
+        }
+        if(x == xy[2] && y == xy[3]) {
+            answer = step;
+            step = 0;
+        }
+
+        BFS(x, y);
     }
 }
 
