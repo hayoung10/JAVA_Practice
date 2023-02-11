3e247224e6ec76b051d18c313b9f960a30014610
hayoung10 <mhymsy@gmail.com>
2023-02-11 11:55:42 +0900

    Programmers (SQL) - Level 3 - 자동차 대여 기록에서 대여중-대여가능 여부 구분하기

---
+++ MySQL/Level3/자동차 대여 기록에서 대여중-대여 가능 여부 구분하기.sql
@@ -0,0 +1,8 @@
+-- Programmers (GROUP BY) 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기
+SELECT CAR_ID, IF(CAR_ID IN
+                  (SELECT CAR_ID
+                   FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+                   WHERE '2022-10-16' BETWEEN START_DATE AND END_DATE), '대여중', '대여 가능') AS AVAILABILITY
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+GROUP BY CAR_ID
+ORDER BY CAR_ID DESC
