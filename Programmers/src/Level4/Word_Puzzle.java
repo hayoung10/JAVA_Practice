8dc1193175b297c85efdf56e48e2f49b2f447ac5
hayoung10 <mhymsy@gmail.com>
2023-03-25 21:16:52 +0900

    Programmers - Level 4 - 단어 퍼즐

---
+++ Programmers/src/Level4/Word_Puzzle.java
@@ -0,0 +1,30 @@
+package Level4;
+
+// DP 동적계획법
+
+public class Word_Puzzle { // 단어 퍼즐
+    public int solution(String[] strs, String t) {
+        int[] dp = new int[t.length()];
+
+        for(int i = 0; i < t.length(); i++) {
+            dp[i] = t.length() + 1;
+
+            for(int j = 0; j < strs.length; j++) {
+                int lastIdx = strs[j].length() - 1;
+
+                if(t.charAt(i) != strs[j].charAt(lastIdx) || i < lastIdx) continue;
+
+                if(strs[j].equals(t.substring(i - lastIdx, i + 1))) {
+                    if(i == lastIdx)
+                        dp[i] = 1;
+                    else
+                        dp[i] = Math.min(dp[i], dp[i - lastIdx - 1] + 1);
+                }
+            }
+        }
+
+        if(dp[t.length() - 1] >= t.length() + 1) return -1;
+
+        return dp[t.length() - 1];
+    }
+}
