d21dcdd59205e62b375566a208d644178090f748
hayoung10 <mhymsy@gmail.com>
2023-06-24 00:56:23 +0900

    Baekjoon - Gold 4 - windows (100%)

--- Baekjoon/src/Gold4/G2044.java
+++ Baekjoon/src/Gold4/G2044.java
@@ -11,28 +11,30 @@ public class G2044 { // windows
         int M = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
 
-        String[] info = new String[N]; // screen-shot을 나타내 주는 정보
-        for(int i = 0; i < M; i++) info[i] = br.readLine();
+        char[][] info = new char[M][N]; // screen-shot을 나타내 주는 정보
+        for(int i = 0; i < M; i++) info[i] = br.readLine().toCharArray();
 
         // window 정보 정리
         ArrayList<Window> windows = new ArrayList<>();
+        int tmp, x, y;
         for(int i = 0; i < M; i++) {
-            String line = info[i];
+            for(int j = 0; j < N; j++) {
+                if (info[i][j] == '+') {
+                    Window window = new Window();
 
-            if(line.contains("-|")) {
-                String title = line.replaceAll("[^a-z]", "");
-                int s = line.indexOf("+");
-                int e = line.indexOf("+", s + 1);
-                int n = e - s + 1;
-                int m = 0;
-                for(int j = i + 1; j < M; j++) {
-                    if(info[j].charAt(s) == '+') {
-                        m = (j - i + 1);
-                        break;
-                    }
+                    for(tmp = j + 3; !Character.isLowerCase(info[i][tmp]); tmp++);
+                    for(y = tmp + 1; Character.isLowerCase(info[i][y]); y++); y--;
+                    window.title = String.copyValueOf(info[i], tmp, y - tmp + 1);
+
+                    for(y += 1; info[i][y] != '+'; y++);
+                    window.n = y - j + 1; //
+
+                    for(x = i + 1; info[x][j] != '+'; x++);
+                    window.m = x - i + 1;
+
+                    windows.add(window);
+                    info[i][j] = info[i][y] = info[x][j] = info[x][y] = '.';
                 }
-
-                windows.add(new Window(title, m, n));
             }
         }
         Collections.sort(windows); // window 정렬
@@ -49,12 +51,12 @@ public class G2044 { // windows
             answer[x1][y1] = answer[x1][y2] = answer[x2][y1] = answer[x2][y2] = '+'; // 모서리
             for(int i = x1 + 1; i < x2; i++) {
                 answer[i][y1] = answer[i][y2] = '|'; // 세로
-                Arrays.fill(answer[i], y1 + 1, y2, '.'); // 창 안
+                Arrays.fill(answer[i], y1 + 1, y2, '.'); // 창 내부
             }
             for(int j = y1 + 1; j < y2; j++) answer[x1][j] = answer[x2][j] = '-'; // 가로
             // 제목
             String str = "|" + window.title + "|";
-            int tmp = (window.n - str.length()) / 2;
+            tmp = (window.n - str.length()) / 2;
             for(int i = 0; i < str.length(); i++)
                 answer[x1][y1 + tmp + i] = str.charAt(i);
         }
@@ -72,12 +74,6 @@ public class G2044 { // windows
     private static class Window implements Comparable<Window> {
         String title;
         int m, n;
-
-        public Window(String title, int m, int n) {
-            this.title = title;
-            this.m = m;
-            this.n = n;
-        }
 
         @Override
         public int compareTo(Window o) {
