b449e90fcf3d54523cd9793386afab273acfdf43
hayoung10 <mhymsy@gmail.com>
2023-08-30 13:50:43 +0900

    Baekjoon - Gold 4 - 육각수 (Refactoring)

--- Baekjoon/src/Gold4/G1229.java
+++ Baekjoon/src/Gold4/G1229.java
@@ -13,7 +13,7 @@ public class G1229 { // 육각수
 
         // 초기화
         int[] dp = new int[N + 1]; // dp[N] : 합이 N이 되는 육각수 개수의 최솟값
-        for(int i = 0; i <= 5; i++) dp[i] = i;
+        for(int i = 0; i <= N; i++) dp[i] = i <= 5 ? i : Integer.MAX_VALUE;
 
         // 육각수 만들기
         ArrayList<Integer> hexagon_List = new ArrayList<>(); // 육각수(hn) 리스트
@@ -25,13 +25,9 @@ public class G1229 { // 육각수
         }
 
         // 육각수 개수의 최솟값 구하기
-        for(int i = 13; i <= N; i++) {
-            int minCnt = Integer.MAX_VALUE;
-            for(int j = 0; j < hexagon_List.size(); j++) {
-                if(hexagon_List.get(j) > i) break;
-                minCnt = Math.min(minCnt, dp[i - hexagon_List.get(j)]);
-            }
-            dp[i] = minCnt + 1;
+        for(int hexagon : hexagon_List) {
+            for(int j = hexagon; j <= N; j++)
+                dp[j] = Math.min(dp[j], dp[j - hexagon] + 1);
         }
         System.out.println(dp[N]);
     }
