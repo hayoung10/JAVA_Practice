0fff77a9c97b152384c83f5019884d59d51e7857
hayoung10 <mhymsy@gmail.com>
2023-10-08 20:57:11 +0900

    Baekjoon - Gold 3 - 앱 (Refactoring)

--- Baekjoon/src/Gold3/G7579.java
+++ Baekjoon/src/Gold3/G7579.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class G7579 { // 앱
     static int N, M, maxCost = 0;
     static int[] m, c;
-    static int[][] dp; // dp[i][j] : A_1에서 A_i까지 앱에서 j의 비용으로 확보할 수 있는 최대의 바이트 수
+    static int[] dp; // dp[j] : j의 비용으로 확보할 수 있는 최대의 바이트 수
 
     private static void input() throws IOException { // 초기화
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,22 +24,19 @@ public class G7579 { // 앱
             c[i] = Integer.parseInt(st.nextToken());
             maxCost += c[i]; // 최대 비용 업데이트
         }
-        dp = new int[N + 1][maxCost + 1];
+        dp = new int[maxCost + 1];
     }
 
     private static int knapsack() { // 배낭 문제 이용
         int cost; // 비용
-        for(int i = 1; i <= N; i++) {
-            for(cost = 0; cost <= maxCost; cost++) {
+        for(int i = 1; i <= N; i++)
+            for(cost = maxCost; cost >= 0; cost--)
                 if(cost >= c[i])
-                    dp[i][cost] = Math.max(dp[i][cost], dp[i - 1][cost - c[i]] + m[i]);
-                dp[i][cost] = Math.max(dp[i][cost], dp[i - 1][cost]);
-            }
-        }
+                    dp[cost] = Math.max(dp[cost], dp[cost - c[i]] + m[i]);
 
         // 최소 비용 구하기
         for(cost = 0; cost <= maxCost; cost++)
-            if(dp[N][cost] >= M) break; // 필요한 메모리 : M 바이트
+            if(dp[cost] >= M) break; // 필요한 메모리 : M 바이트
         return cost;
     }
 
