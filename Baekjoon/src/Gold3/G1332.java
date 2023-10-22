7e74b12c5bccd5c11594a3593a45d48984f3633b
hayoung10 <mhymsy@gmail.com>
2023-10-22 17:54:57 +0900

    Baekjoon - Gold 3 - 풀자 (Refactoring)

--- Baekjoon/src/Gold3/G1332.java
+++ Baekjoon/src/Gold3/G1332.java
@@ -6,22 +6,24 @@ import java.util.*;
 public class G1332 { // 풀자
     static int N, V;
     static int[] interest;
-    static int ans = Integer.MAX_VALUE;
 
-    private static void solution(int idx, int minV, int maxV, int cnt) {
-        if(idx >= N) return;
-        if(cnt >= ans) return;
+    private static int solution() { // Brute-Force 이용
+        int min = interest[0];
+        int max = interest[0];
 
-        minV = Math.min(minV, interest[idx]);
-        maxV = Math.max(maxV, interest[idx]);
+        int ret = Integer.MAX_VALUE;
+        for(int i = 1; i < N; i++) {
+            if(interest[i] < min) min = interest[i];
+            else if(interest[i] > max) max = interest[i];
 
-        if(maxV - minV >= V) {
-            ans = Math.min(ans, cnt);
-            return;
+            if(max - min >= V) {
+                for(int j = 0; j < i; j++)
+                    if(Math.abs(interest[i] - interest[j]) >= V)
+                        ret = Math.min(ret, (j + 1) / 2 + 1 + (i - j) + 1 / 2);
+                return ret;
+            }
         }
-
-        solution(idx + 1, minV, maxV, cnt + 1);
-        solution(idx + 2, minV, maxV, cnt + 1);
+        return N;
     }
 
     public static void main(String[] args) throws IOException {
@@ -32,16 +34,9 @@ public class G1332 { // 풀자
         V = Integer.parseInt(st.nextToken());
         interest = new int[N];
 
-        int maxI = Integer.MIN_VALUE, minI = Integer.MAX_VALUE;
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) {
-            interest[i] = Integer.parseInt(st.nextToken());
-            maxI = Math.max(maxI, interest[i]);
-            minI = Math.min(minI, interest[i]);
-        }
+        for(int i = 0; i < N; i++) interest[i] = Integer.parseInt(st.nextToken());
 
-        if(maxI - minI < V) ans = N;
-        else solution(0, 1000, -1000, 1);
-        System.out.println(ans);
+        System.out.println(solution());
     }
 }
