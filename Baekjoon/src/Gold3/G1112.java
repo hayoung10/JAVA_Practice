f6cbf2b23e27b8a26177cf9ba90ad40ee99512ef
hayoung10 <mhymsy@gmail.com>
2023-11-04 12:11:08 +0900

    Baekjoon - Gold 3 - 진법 변환 (100%)

--- Baekjoon/src/Gold3/G1112.java
+++ Baekjoon/src/Gold3/G1112.java
@@ -5,6 +5,7 @@ import java.util.*;
 
 public class G1112 { // 진법 변환
     static StringBuilder ans = new StringBuilder();
+    static boolean flag = false; // 음수를 양의 진법으로 바꿀 때
 
     private static void calculate(int x, int b) {
         int quot = x / b; // 몫
@@ -14,8 +15,11 @@ public class G1112 { // 진법 변환
             remain = x - b * quot;
         }
         ans.append(remain);
-        
-        if(quot == 0) return;
+
+        if(quot == 0) {
+            if(flag) ans.append("-");
+            return;
+        }
         calculate(quot, b);
     }
 
@@ -23,6 +27,10 @@ public class G1112 { // 진법 변환
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
+        if(input[0] < 0 && input[1] > 0) {
+            flag = true;
+            input[0] *= -1;
+        }
         calculate(input[0], input[1]); // calculate(x, b)
         System.out.println(ans.reverse());
     }
