b106efa0b4c8f6916584653499c7a00b838c9b9f
hayoung10 <mhymsy@gmail.com>
2023-11-19 14:21:02 +0900

    Baekjoon - Gold 3 - 색종이 - 3 (Refactoring)

--- Baekjoon/src/Gold3/G2571.java
+++ Baekjoon/src/Gold3/G2571.java
@@ -7,21 +7,22 @@ public class G2571 { // 색종이 - 3
     static int[][] board = new int[101][101];
 
     private static int prefixSum() { // 누적 합 이용
-        int[][] dp = new int[101][101];
-        for(int i = 1; i < 101; i++)
+        // 높이 누적 합 구하기
+        for(int i = 1; i < 100; i++)
             for(int j = 1; j < 101; j++)
-                dp[i][j] = board[i][j] + (dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1]);
+                if(board[i][j] != 0 && board[i + 1][j] != 0)
+                    board[i + 1][j] = board[i][j] + 1;
 
-        // 직사각형의 최대 넓이 구하기 (직사각형의 크기 : (i, j) ~ (m, n))
+        // 직사각형의 최대 넓이 구하기
         int ret = 0;
         for(int i = 1; i < 101; i++) {
             for (int j = 1; j < 101; j++) {
-                for (int m = i + 1; m < 101; m++) {
-                    for (int n = j + 1; n < 101; n++) {
-                        int area = dp[m][n] - (dp[m][j - 1] + dp[i - 1][n] - dp[i - 1][j - 1]);
-                        if(area < 0) break;
-                        ret = Math.max(ret, area);
-                    }
+                int h = 100;
+
+                for(int k = j; k < 101; k++) {
+                    h = Math.min(h, board[i][k]);
+                    if(h == 0) break;
+                    ret = Math.max(ret, h * (k - j + 1));
                 }
             }
         }
@@ -32,7 +33,6 @@ public class G2571 { // 색종이 - 3
         // 초기화
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int cnt = Integer.parseInt(br.readLine()); // 색종이의 수
-        for(int[] b : board) Arrays.fill(b, -10001);
         StringTokenizer st;
         for(int i = 0; i < cnt; i++) {
             st = new StringTokenizer(br.readLine());
